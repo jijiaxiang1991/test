23120a3fbf61858ede35547f16be26f63a031f47
jijiaxiang1991 <jijiaxiangwell@163.com>
2016-08-25 10:27:48 +0800

    Update test.java

--- src/com/atguigu/test.java
+++ src/com/atguigu/test.java
@@ -6,6 +6,7 @@ public class test {
 		System.out.println("hello");
 		System.out.println("v2");
 		System.out.println("v1");
-		system.out.println("123");
+		System.out.println("123");
+		System.out.println("aaa");
 	}
 }
